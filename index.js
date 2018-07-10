8f4211671f4e9dc50bb0a1806d8928f600ab50e3
qaisaru@icloud.com <qaisaru@icloud.com>
2018-07-10 12:11:25 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-// add solution here
+function theBeatlesPlay(musicians, instruments){
+  const arr=[];
+  
+ for (let i = 0, l = musicians.length; i < l; i++){
+     arr.push(`${musicians[i]} plays ${instruments[i]}`)
+ }
+ return arr
+}
