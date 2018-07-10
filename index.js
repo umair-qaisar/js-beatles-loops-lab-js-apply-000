864cc188e9298a1bd85a365916bec6352facc667
qaisaru@icloud.com <qaisaru@icloud.com>
2018-07-10 12:15:48 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -7,4 +7,11 @@ function theBeatlesPlay(musicians, instruments){
  return arr
 }
 
+const facts = [
+  "He was the last Beatle to learn to drive",
+  "He was never a vegetarian",
+  "He was a choir boy and boy scout",
+  "He hated the sound of his own voice"
+];
+
 function johnLennonFacts(){}
